1a76bf8201ac17a6db83d1b774a7f2b02df9807e
Darrin Weng <darrin@max-ubuntu1004.(none)>
2011-12-05 20:23:38 -0800

    Added a helpful help message

--- parseDNAFile.py
+++ parseDNAFile.py
@@ -65,16 +65,17 @@ primerSequence = "AACCTT"
 def main():
   
   #Parse Command line
-  parser = OptionParser()
-  parser.add_option("-p", "--path", dest="dir", default="Genome Sequences/rDNA plasmid sequences/23-5/")
-  parser.add_option("-d", dest="DisSeq", default="AACACGCGA23(GATC)GAA")
-  parser.add_option("--max", dest="max", type="int", default=-1)
+  helpmsg = "Takes Genome Sequences from DIR and generates all posible pyroprints.\nThen produces a pearson correlation and displays the results"
+  parser = OptionParser(usage=helpmsg)
+  parser.add_option("-p", "--path", dest="dir", default="Genome Sequences/rDNA plasmid sequences/23-5/", help="Path to Genome Sequence Folders")
+  parser.add_option("-d", dest="DispSeq", default="AACACGCGA23(GATC)GAA", help="Dispination order")
+  parser.add_option("-m", "--max", dest="max", type="int", default=-1, help="Max number of combonations to generate")
 
   (options, args) = parser.parse_args()
   dataPath = options.dir
   comboLimit = options.max
   
-  dispSeq = buildDispSeq(options.DisSeq)
+  dispSeq = buildDispSeq(options.DispSeq)
   
   #Find all files in Dir
   path = dataPath
